2ec9267eb45a929aaf3bcc183e46ea823fdb75c4
Darrel Jackson <darrel.jackson@ttu.edu>
2021-03-29 18:35:21 -0500

    Navbar
    The navbar has been implemented

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,9 +4,7 @@ import { Container } from 'reactstrap';
 const Layout = (props) => {
 	return (
 		<React.Fragment>
-			<Container>
 				{props.children}
-			</Container>
 		</React.Fragment>
 	);
 }
